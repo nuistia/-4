f416a94216522576307bd5b80e1f37fa8f5f34d5
Anastasia <nuistia@gmail.com>
2022-04-04 19:26:47 +0300

    Main calc

---
+++ calc.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a, b, op;   double result;
+
+    cout << "¬вед≥ть перше число\n";   cin >> a;
+
+    cout << "¬вед≥ть оператор\n1\t+\n2\t-\n3\t*\n4\t/\n\n";  cin >> op;
+
+    cout << "¬вед≥ть друге число\n";     cin >> b;
+
+    switch (op) {
+
+    case 1:  result = a + b;   // code
+
+        break;
+
+    case 2: result = a - b;   // code
+
+        break;
+
+    case 3: result = a * b  // code
+
+        break;
+
+    case 4:   result = a / b   // code
+
+        break;
+
+    }
+
+    cout << "–езультат = " << result;
+
+}
